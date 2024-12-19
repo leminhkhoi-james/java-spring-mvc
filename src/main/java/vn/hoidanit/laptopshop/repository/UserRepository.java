d7ad5c0aaed79fc0b7b52d1da507018b2021d558
“Khoi” <leminhkhoi16062k5@gmail.com>
2024-12-19 15:04:06 +0700

    Repository Query

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package vn.hoidanit.laptopshop.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vn.hoidanit.laptopshop.domain.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     User save(User hoidanit);
+
+    List<User> findByEmail(String email);
 }
